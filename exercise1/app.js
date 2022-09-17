dd3258259c16d4212bc8be4ec919f3cb3ea6b5ee
mohsin-8 <alikhanmohsin420@gmail.com>
2022-09-17 02:12:36 -0700

    first exercise

---
+++ exercise1/app.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+
+/* 
+Exercise1: Personal Message: Store a person’s name in a variable,
+and print a message to that person. Your message should be 
+simple, such as, “Hello Eric, would you like to learn some Python today?”
+*/
+
+let person_name = "Mohsin";
+
+document.write(
+  `Hello ${person_name}, would you like to learn some Web3.0 topics today?`
+);
