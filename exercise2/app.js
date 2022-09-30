5e006d8b0811014730bf66858eab50f63cea380a
mohsin-8 <alikhanmohsin420@gmail.com>
2022-09-30 05:49:57 -0700

    exercise 3

--- exercise2/app.js
+++ exercise2/app.js
@@ -1,7 +1,8 @@
 console.log("Hello World");
 
 /*
-exercise2: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+exercise2: Name Cases: Store a person’s name in a variable,
+and then print that person’s name in lowercase, uppercase, and titlecase.
 */
 
 let person_name;
