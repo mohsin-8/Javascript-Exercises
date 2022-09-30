25a2beaba8fa9838f9b0733b1369a848d2dc91d9
mohsin-8 <alikhanmohsin420@gmail.com>
2022-09-30 06:50:16 -0700

    exercise 5

---
+++ exercise5/app.js
@@ -0,0 +1,13 @@
+// console.log("Hello World");
+
+/*
+Exerice 5: Stripping Names: Store a person’s name, and include 
+some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, "\t" and "\n", at least once. 
+Print the name once, so the whitespace around the name 
+is displayed. Then print the name after striping the white spaces.
+*/
+
+let myName = " \tMohsin Ali Khan\n ";
+document.write("Answer in console");
+console.log(myName);
