d64e4bcc72edf5c15f1e44efb06fa4256fb400d4
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-01 09:14:15 -0700

    exercise 12

---
+++ exercise12/app.js
@@ -0,0 +1,16 @@
+// console.log("Hello World");
+
+/*
+Guest List: If you could invite anyone, living or deceased, to dinner, 
+who would you invite? Make a list that includes at least three people 
+you’d like to invite to dinner. Then use your list to print 
+a message to each person, inviting them to dinner.
+*/
+
+const guestList = ["Mohsin", "Ali", "Khan"];
+
+guestList.map((guest) => {
+  //   console.log(`Hey! ${guest}, we are planing dinner tonight`);
+
+  document.write(`Hey! ${guest}, we are planing dinner tonight` + "<br />");
+});
