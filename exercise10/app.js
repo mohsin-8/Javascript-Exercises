c2ebed22c1fd35f9d7595555464ff2661b41d725
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-01 08:10:12 -0700

    exercise 10

---
+++ exercise10/app.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+/*
+Greetings: Start with the array you used in Exercise 11, 
+but instead of just printing each person’s name, print a message to them. The text of each 
+message should be the same, but each message should be personalized with the person’s name. 
+*/
+
+const names = ["Mohsin", "Ali", "Khan"];
+
+let mesg = "is a talented student";
+
+document.write(names[0] + " " + mesg + "<br />");
+document.write(names[1] + " " + mesg + "<br />");
+document.write(names[2] + " " + mesg + "<br />");
--- exercise9/app.js
+++ exercise9/app.js
@@ -1,7 +1,7 @@
 // console.log("Hello World");
 
 /*
-Names: Store the names of a few of your friends in a array 
+Exercise 10: Names: Store the names of a few of your friends in a array 
 called names. Print each person’s name by accessing each 
 element in the list, one at a time.
 */
