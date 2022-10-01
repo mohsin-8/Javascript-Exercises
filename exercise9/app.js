0926e1c054f8b3610a1470e05db4e89415fd1a88
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-01 08:01:40 -0700

    exercise 9

---
+++ exercise9/app.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+
+/*
+Names: Store the names of a few of your friends in a array 
+called names. Print each person’s name by accessing each 
+element in the list, one at a time.
+*/
+
+const names = ["Mohsin", "Ali", "Khan"];
+
+// names[0];
+
+document.write(names[0] + "<br />");
+document.write(names[1] + "<br />");
+document.write(names[2]);
+
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
