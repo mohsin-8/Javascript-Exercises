0ce032b0de233c09bf1308223add5e478ef27f52
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-02 09:05:42 -0700

    complete exercise29

---
+++ exercise27/app.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+/*
+Favorite Fruit: Make a array of your favorite fruits, 
+and then write a series of independent if statements 
+that check for certain fruits in your array.
+
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. 
+If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+*/
+
+let favourite_fruits = ["Apple", "Mango", "Banana"];
+// console.log(favourite_fruits);
+
+favourite_fruits.map((fruit) => {
+  if (favourite_fruits.includes(fruit)) console.log(`you really like ${fruit}`);
+});
