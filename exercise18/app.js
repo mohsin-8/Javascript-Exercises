3773198d8667185d35e24097234db5fc954c3700
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-02 03:11:41 -0700

    complete exercise 20

---
+++ exercise18/app.js
@@ -0,0 +1,23 @@
+// console.log("Hello World");
+
+/*
+Think of something you could store in a array. 
+For example, you could make a list of mountains, rivers, countries, 
+cities, languages, or anything else you’d like. Write a 
+program that creates a list containing these items.
+*/
+
+let countries = [
+  "Pakistan",
+  "Italy",
+  "Germany",
+  "USA",
+  "Netherland",
+  "Azerbaijan",
+  "Cyprus",
+  "Argentina",
+];
+
+console.log(countries);
+
+console.log(`Countries List: ${countries.length}`);
