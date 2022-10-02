5c659e784cc9a2bbdd8385542e0ee303f9d0dda9
mohsin-8 <alikhanmohsin420@gmail.com>
2022-10-02 13:07:31 -0700

    complete exercise 35, 26

---
+++ exercise34/app.js
@@ -0,0 +1,15 @@
+// console.log("Hello world");
+
+/*
+T-Shirt: Write a function called make_shirt() that accepts a size and the 
+text of a message that should be printed on the shirt. The function should 
+print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+*/
+
+function make_shirt(size, mesg) {
+  console.log(
+    `T-shirt size is '${size}' and the message that will print on it is '${mesg}'`
+  );
+}
+
+make_shirt("medium", "Panaverse");
